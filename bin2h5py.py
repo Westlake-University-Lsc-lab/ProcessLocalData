97a2cfc61a164eb2d86917db81e90e1aec5e78f2
yangjijun <yangjijun@westlake.edu.cn>
2024-08-30 15:49:00 +0800

    changde area integral with fixed range 100 too 180 sample range

--- bin2h5py.py
+++ bin2h5py.py
@@ -15,7 +15,7 @@ path_save =""
 with open(file_list, 'r') as list:
     for line in list:  
         winfo =[]
-        rawfilename = line.rstrip('\n')[17 :] 
+        rawfilename = line.rstrip('\n') # [17 :] 
         rawdata = daw_readout.DAWDemoWaveParser(rawfilename)     
         for wave in tqdm(rawdata) :
             ch = wave.Channel
@@ -24,33 +24,22 @@ with open(file_list, 'r') as list:
             pulse = wave.Waveform
             st, minp, ed = process_data.pusle_index(pulse)
             ht = base - wave.Waveform[minp]
-            area = process_data.pusle_area(pulse, st, ed, base)
-            area_fix_len = process_data.pulse_area_fix_len(pulse, minp, 15, base)
-            area_fix_range = process_data.pulse_area_fix_range(pulse, 95, 130, base)
-            area_fix_range_dy = process_data.pulse_area_fix_range(pulse, 90, 150, base)
-            if ch == 0:
-                area_pe = area / 77.791
-                area_fix_len_pe = area_fix_len / 77.791
+            area_fix_range = process_data.pulse_area_fix_range(pulse, 100, 180, base)
+            if ch == 0:  ### LV2414 Anode
                 area_fix_range_pe = area_fix_range / 77.791
             if ch == 1:
-                area_pe = area / 96.99
-                area_fix_len_pe = area_fix_len / 96.99
                 area_fix_range_pe = area_fix_range / 96.99
-            if ch == 2:
-                area_pe = area / 77.791
-                area_fix_len_pe = area_fix_len / 77.791
-                area_fix_range_pe = area_fix_range_dy / 77.791 
+            if ch == 2:   #### LV2414 Dynode
+                area_fix_range_pe = area_fix_range / 77.791
             winfo.append({
                 'Ch':ch,
                 'TTT':ttt,   ## Trigger time tag
                 'Baseline': base, 
                 'Hight': ht, 
-                'Area': area_pe,     ## area of pulse cal by dynamic range
-                'Area_fixlen': area_fix_len_pe,  ## area of pulse cal by fix length 10
-                'Area_fixrange':area_fix_range_pe
-                #'Wave': pulse,
+                'Area_fixrange':area_fix_range_pe,
             })
-        file_tag = line.rstrip('\n')[17 :].rstrip('.bin')[24:][: -12]  
+        #file_tag = line.rstrip('\n')[17 :].rstrip('.bin')[24:][: -12]  
+        file_tag = line.rstrip('\n').rstrip('.bin')[24:][: -12]  
         path_save = "outnpy/{}.h5py".format(file_tag)
         df = pd.DataFrame(winfo)
         process_data.write_to_hdf5(df, path_save)
