281f92a1f540ed7f8dba6c07837bc413f2537326
yangjijun <yangjijun@westlake.edu.cn>
2024-10-14 20:17:39 +0800

    fixed  PMT gain on unit of ADC and  attenuator factor

--- bin2h5py.py
+++ bin2h5py.py
@@ -12,6 +12,13 @@ argvs = sys.argv
 file_list =  sys.argv[1] 
 path_save =""
 
+gain_lv2414 = 77.791   # convertered to ADC  
+gain_lv2415 = 96.99    # convertered to ADC  
+attenuation_factor_9DB = 2.76   # attenuation factor 
+attenuation_factor_6DB = 1.87   # attenuation factor 
+attenuation_factor_12DB = 3.85  # attenuation factor 
+attenuation_factor_18DB = 7.62   # attenuation factor 
+
 with open(file_list, 'r') as list:
     for line in list:  
         winfo =[]
@@ -24,16 +31,16 @@ with open(file_list, 'r') as list:
             pulse = wave.Waveform
             st, minp, ed = process_data.pusle_index(pulse)
             ht = base - wave.Waveform[minp]
-            area_fix_range = process_data.pulse_area_fix_range(pulse, 90, 140, base)
-            area_fix_range_dy = process_data.pulse_area_fix_range(pulse, 90, 200, base)
+            area_fix_range = process_data.pulse_area_fix_range(pulse, 90, 3000, base)
+            #area_fix_range_dy = process_data.pulse_area_fix_range(pulse, 90, 300, base)
             if ch == 0:  ### LV2414 Anode
-                ht = ht * 2.76
-                area_fix_range_pe = area_fix_range / 77.791 *2.76
+                ht = ht * attenuation_factor_9DB
+                area_fix_range_pe = area_fix_range / (gain_lv2414 *attenuation_factor_9DB)
             if ch == 1:
-                area_fix_range_pe = area_fix_range / 96.99
-                area_fix_range_pe = area_fix_range / 96.99
+                area_fix_range_pe = area_fix_range / gain_lv2415
+                area_fix_range_pe = area_fix_range / gain_lv2415
             if ch == 2:   #### LV2414 Dynode
-                area_fix_range_pe = area_fix_range_dy / 77.791
+                area_fix_range_pe = area_fix_range / gain_lv2414
             winfo.append({
                 'Ch':ch,
                 'TTT':ttt,   ## Trigger time tag
