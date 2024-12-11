085d8350df8ee63b126963cbea675c131849cb20
yangjijun <yangjijun@westlake.edu.cn>
2024-12-11 22:26:32 +0800

    package to contain all constant paramters

---
+++ constant.py
@@ -0,0 +1,14 @@
+
+gain_lv2414 = 77.791   # unit in ADC  
+gain_lv2415 = 96.99    # unit in ADC  
+
+attenuation_factor_6DB = 1.87   # attenuation factor 
+attenuation_factor_9DB = 2.76   # attenuation factor 
+attenuation_factor_12DB = 3.85  # attenuation factor 
+attenuation_factor_18DB = 7.62   # attenuation factor 
+attenuation_factor_20DB = 9.86
+
+Rft = 83.55963261815212
+Rft_err= 2.611696693187247
+GR_mu=121.26085465658046
+GR_err=2.729860386508014
