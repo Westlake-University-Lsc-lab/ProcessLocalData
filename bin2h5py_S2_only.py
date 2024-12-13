92b2f9ead56335515ad027ad5e4a4d8cf08e6ec1
yangjijun <yangjijun@westlake.edu.cn>
2024-12-14 00:40:58 +0800

    main code to process binary data

--- bin2h5py_S2_only.py
+++ bin2h5py_S2_only.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 import sys
 import time
 import os
+import constant
 
 argvs = sys.argv
 file_list =  sys.argv[1] 
@@ -14,48 +15,55 @@ processed_list = []
 path_save =""
 
 
-gain_lv2414 = 77.791   # convertered to ADC  
-gain_lv2415 = 96.99    # convertered to ADC  
-attenuation_factor_9DB = 2.76   # attenuation factor 
-attenuation_factor_6DB = 1.87   # attenuation factor 
-attenuation_factor_12DB = 3.85  # attenuation factor 
-attenuation_factor_18DB = 7.62   # attenuation factor 
-
+gain_lv2414 = constant.gain_lv2414 
+gain_lv2415 = constant.gain_lv2415 
+attenuation_factor_9DB = constant.attenuation_factor_9DB 
+attenuation_factor_6DB = constant.attenuation_factor_6DB 
+attenuation_factor_12DB = constant.attenuation_factor_12DB 
+attenuation_factor_18DB = constant.attenuation_factor_18DB 
+attenuation_factor_20DB = constant.attenuation_factor_20DB 
 
 with open(file_list, 'r') as list:
     for line in list:  
         winfo =[]
         rawfilename = line.rstrip('\n') # [17 :] 
-        delt_t = rawfilename.split('680mv_')[1].split('us_50hz')[0]
+        delta_t = rawfilename.split('680mv_')[1].split('us_50hz')[0]
+        # voltage = rawfilename.split('20241212_')[1].split('v_S2_OFF')[0].replace('p','.')        
+        # voltage = rawfilename.split('20241212_')[1].split('v_calibration')[0].replace('p','.')        
         rawdata = daw_readout.DAWDemoWaveParser(rawfilename)     
         for wave in tqdm(rawdata) :
             ch = wave.Channel
             ttt = wave.Timestamp
-            base = wave.Baseline
-            base_l20 = np.mean(wave.Waveform[ -20:])
-            if ch == 0 :
-                if delt_t == '1': 
-                    base = base_l20                  
-                if delt_t == '2' : 
-                    base = base_l20
-                # print(r'this ch={}, baseline:{}, base_l20:{}'.format(ch, base, base_l20))                  
+            base = wave.Baseline            
+            if ch == 0 and delta_t == '1' :
+                base = 15171
+            if ch == 2 and delta_t == '1' :
+                base =  8112
             pulse = wave.Waveform
-            area = process_data.pulse_area_fix_range(pulse, 100, 370, base)
+            # area = process_data.pulse_area_fix_range(pulse, 50, 370, base) ### for charge calibration
+            area = process_data.pulse_area_fix_range(pulse, 100, 370, base)   ## for S2 calibration
+            # area = process_data.pulse_area_fix_range(pulse, 350, 620, base)   ## for dt=1us S2 calibration
+            # area = process_data.pulse_area_fix_range(pulse, 600, 870, base)   ## for df=2us S2 calibration
             if ch == 0:
+                # area = area / gain_lv2414 *attenuation_factor_20DB
+                # area = area / gain_lv2414 *attenuation_factor_9DB
                 area = area / gain_lv2414 *attenuation_factor_12DB
+                # area = area / gain_lv2414              
             if ch == 1:
-                area = area / gain_lv2415 * attenuation_factor_6DB
+                # area = area / gain_lv2415 * attenuation_factor_12DB
+                # area = area / gain_lv2415 * attenuation_factor_9DB
+                area = area / gain_lv2415                
             if ch == 2:
                 area = area / gain_lv2414
             winfo.append({         
                 'Ch':ch,
                 'TTT':ttt,   
                 'Baseline': base, 
-                'base_l20': base_l20,               
                 'Area_S2':area,
                 'S1_width':150, #ns
-                'S2_width':1000, #ns  5us, 90us, 200us
-                'Delta_t': int(delt_t), #us
+                'S2_width':1000, #ns 
+                'Delta_t': int(delta_t), #us
+                # 'Voltage': float(voltage),
                 'Wave': pulse
             })    
         #file_tag = line.rstrip('\n')[17 :].rstrip('.bin')[24:][: -12]  
