7d47d3650f19850d58615c9c7c346cd03663d481
yangjijun <yangjijun@westlake.edu.cn>
2025-01-03 17:48:07 +0800

    updated func

--- fit_package.py
+++ fit_package.py
@@ -18,9 +18,10 @@ def check_type(input_numb):
             print("Please input a valid parameter type.")
 
 def ftag(file):
-    fdate = file.split('LED_')[0].split('lv2414_')[1]
-    led_config = file.split('LED_')[1].split('.h5py')[0]
-    file_tag = fdate + led_config
+    file_tag = file.split('outnpy/')[1].split('.h5py')[0]
+    # fdate = file.split('LED_')[0].split('lv2414_')[1]
+    # led_config = file.split('LED_')[1].split('.h5py')[0]
+    # file_tag = fdate + led_config
     return file_tag
 
 def read_hdfile(file):
@@ -69,8 +70,10 @@ def fit_single_channel(df, channel, ftag):
         area = -area
         mean = np.mean(area)
         std = np.std(area)
-        ledge = 0
         redge = np.max(area)
+        ledge = np.min(area)
+        if ledge < 0:
+            ledge = redge /2.      
         amp = len(area)
     elif channel != 2:       
         mean = np.mean(area)
@@ -91,6 +94,66 @@ def fit_single_channel(df, channel, ftag):
         xlabel=(r'Ch{}Area (PE)'.format(channel)),title=tile,Save=False)
     return s2_mu, s2_sigma
     
+def fit_S1_channel(df, channel, ftag):
+    if check_type(channel) ==True:
+        pass
+    else:
+        print("Please input a valid parameter type with interger value.")
+        sys.exit()   
+    area = df.Area_S1[df.Ch == channel].astype(np.float64).to_numpy()
+    if channel == 2:
+        area = -area
+        mean = np.mean(area)
+        std = np.std(area)
+        redge = np.max(area)
+        ledge = np.min(area)
+        if ledge < 0:
+            ledge = redge /2.      
+        amp = len(area)
+    elif channel != 2:       
+        mean = np.mean(area)
+        std = np.std(area)
+        ledge = np.min(area)
+        redge = np.max(area)    
+        amp = len(area)
+    nbins = 100              
+    if channel == 0:
+        pmt = 'LV1414'
+    elif channel == 1:
+        pmt = 'LV2415'
+    elif channel == 2:
+        pmt = 'LV2414 Dynode'
+    tile = r'{}'.format(pmt)
+    s1_mu, s1_sigma =  analysis_data.plot_fit_histgram_vs_Gaussion(
+        area,nbins,ledge,redge,p0=[amp,mean,std],file_tag=ftag, 
+        xlabel=(r'Ch{}Area (PE)'.format(channel)),title=tile,Save=False)
+    return s1_mu, s1_sigma
+
+
+def fit_baseline(df, channel, ftag):
+    if check_type(channel) ==True:
+        pass
+    else:
+        print("Please input a valid parameter type with interger value.")
+        sys.exit()   
+    base = df.Baseline[df.Ch == channel].astype(np.float64).to_numpy()
+    mean = np.mean(base)
+    std = np.std(base)
+    redge = np.max(base)
+    ledge = np.min(base)
+    amp = len(base)
+    nbins = 5           
+    if channel == 0:
+        pmt = 'LV1414'
+    elif channel == 1:
+        pmt = 'LV2415'
+    elif channel == 2:
+        pmt = 'LV2414 Dynode'
+    tile = r'{}'.format(pmt)
+    base_mu, base_sigma =  analysis_data.plot_fit_histgram_vs_Gaussion(
+        base,nbins,ledge,redge,p0=[amp,mean,std],file_tag=ftag, 
+        xlabel=(r'Ch{}Area (PE)'.format(channel)),title=tile,Save=False)
+    return base_mu, base_sigma
    
 import analysis_data
 def calculate_wf_data_array(file_list, Channel='Anode'):
@@ -102,49 +165,32 @@ def calculate_wf_data_array(file_list, Channel='Anode'):
             file = line.rstrip('\n')
             time_space=file.split('680mv_')[1].split('_50hz')[0]
             led_config=file.split('combine_')[0]+'combine_'+file.split('combine_')[1].split('680mv_')[0]+'680mv_'
-            if time_space == '1us':
-                mean_1us, std_1us = analysis_data.calculate_wf_mean_std_s2(file, threshold=100,  Channel=Channel)
-                waveform_dictionary['1us'] = {'mean_wf':mean_1us, 'std_wf':std_1us}
-            elif time_space == '2us':
-                mean_2us, std_2us = analysis_data.calculate_wf_mean_std_s2(file, threshold=100,  Channel=Channel)
-                waveform_dictionary['2us'] = {'mean_wf':mean_2us, 'std_wf':std_2us}
-            elif time_space == '5us':
-                mean_5us, std_5us = analysis_data.calculate_wf_mean_std_s2(file, threshold=100,  Channel=Channel)
-                waveform_dictionary['5us'] = {'mean_wf':mean_5us, 'std_wf':std_5us}
-            elif time_space == '10us':
-                mean_1us, std_1us = analysis_data.calculate_wf_mean_std_s2(file, threshold=100, Channel=Channel)
-                waveform_dictionary['1us'] = {'mean_wf':mean_1us, 'std_wf':std_1us}
-            elif time_space == '20us':
-                mean_20us, std_20us = analysis_data.calculate_wf_mean_std_s2(file, threshold=100,Channel=Channel)
-                waveform_dictionary['20us'] = {'mean_wf':mean_20us, 'std_wf':std_20us}
-            elif time_space == '50us':
-                mean_50us, std_50us = analysis_data.calculate_wf_mean_std_s2(file, threshold=100,  Channel=Channel)
-                waveform_dictionary['50us'] = {'mean_wf':mean_50us, 'std_wf':std_50us}
-            elif time_space == '100us':
-                mean_100us, std_100us = analysis_data.calculate_wf_mean_std_s2(file, threshold=100,  Channel=Channel)
-                waveform_dictionary['100us'] = {'mean_wf':mean_100us, 'std_wf':std_100us}
-            elif time_space == '200us' :
-                mean_200us, std_200us = analysis_data.calculate_wf_mean_std_s2(file, threshold=100,  Channel=Channel)
-                waveform_dictionary['200us'] = {'mean_wf':mean_200us, 'std_wf':std_200us}
-            elif time_space == '500us':
-                mean_500us, std_500us = analysis_data.calculate_wf_mean_std_s2(file, threshold=100,  Channel=Channel)
-                waveform_dictionary['500us'] = {'mean_wf':mean_500us, 'std_wf':std_500us}
-            elif time_space == '1ms' :
-                mean_1ms, std_1ms = analysis_data.calculate_wf_mean_std_s2(file, threshold=100,  Channel=Channel)
-                waveform_dictionary['1ms'] = {'mean_wf':mean_1ms, 'std_wf':std_1ms}
-            elif time_space == '10ms' :
-                mean_10ms, std_10ms = analysis_data.calculate_wf_mean_std_s2(file, threshold=100,  Channel=Channel)    
-                waveform_dictionary['10ms'] = {'mean_wf':mean_10ms, 'std_wf':std_10ms}
-            elif time_space == '1000us':
-                mean_1000us, std_1000us = analysis_data.calculate_wf_mean_std_s2(file, threshold=100,  Channel=Channel)
-                waveform_dictionary['1000us'] = {'mean_wf':mean_1000us, 'std_wf':std_1000us}
-            elif time_space == '10000us':
-                mean_10000us, std_10000us = analysis_data.calculate_wf_mean_std_s2(file, threshold=100, Channel=Channel)
-                waveform_dictionary['10000us'] = {'mean_wf':mean_10000us, 'std_wf':std_10000us}
+            mean_, std_ = analysis_data.calculate_wf_mean_std_s2(file, threshold=100,  Channel=Channel)
+            waveform_dictionary[time_space] = {'mean_wf':mean_, 'std_wf':std_}
     led_config = led_config.split('lv2414_')[1]
     print(led_config)  
     
     return waveform_dictionary, led_config   
+
+def wf_array_S1(file_list, Channel='Anode'):
+    # time_delay_map = {'5us':5, '200us':200, '1ms':1000, '10ms':10000}  ## time delay unit on 'us'
+    base_config = []
+    waveform_dictionary={}
+    Rd7 = ''
+    with open(file_list, 'r') as list:
+        for line in list: 
+            file = line.rstrip('\n')
+            config_tag=file.split('base_')[1].split('_run')[0]
+            index = file.find('reference') 
+            if index != -1:
+                Rd7 = 'Ref'
+            elif index == -1:             
+                Rd7 = file.split('Rd7_')[1].split('_run')[0]
+            mean_, std_ = analysis_data.calculate_wf_mean_std_s2(file, threshold=100, Channel=Channel)
+            waveform_dictionary[Rd7] = {'mean_wf':mean_, 'std_wf':std_}
+            # print(Rd7)           
+            base_config.append(config_tag) 
+    return waveform_dictionary, base_config  
 
 import numpy as np
 import matplotlib.pyplot as plt
